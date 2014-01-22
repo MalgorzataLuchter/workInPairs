0bb734e31cf4fe19db9ca43f638d368921617998
Malgorzata Luchter <malgorzata.luchter@gmail.com>
2014-01-22 17:11:29 +0100

    zmiany na int

--- utils.h
+++ utils.h
@@ -21,3 +21,24 @@ int sumValue(int* tab, int size){
 	return suma;
 	
 }
+void maxi(int *wsk, int rozmiar){
+ 
+   int *maxi;
+   maxi=wsk;   // ustawienie maxi na pocz¹tek tablicy
+   int *mini;
+   mini=wsk;   
+ 
+    for(int i=0; i<rozmiar;i++,wsk++){
+        if ( *(maxi) < *(wsk)  )   {
+        (maxi) = (wsk);
+   }
+ 
+   else if ( *(mini)  > *(wsk) )
+ mini = wsk;
+}
+ 
+ 
+  cout<<"maksymalna wartosc to:"<<*maxi<<endl;   
+   cout<<"minimalna wartosc to:"<<*mini<<endl;         
+ 
+}
