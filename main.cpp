455eb6d6433c64a7412b7bdbc35703f257e8b73d
teresa <teresakedzior1@gmail.com>
2014-01-22 17:14:09 +0100

    zmiany w main

--- main.cpp
+++ main.cpp
@@ -1,4 +1,17 @@
+#include "max.h"
+#include "min.h"
+
 int main()
 {
-return 0;
+
+cout << endl << "Suma elementow tablicy wynosi: " << suma << endl;
+cout << "Srednia elementow tablicy wynosi: " << (suma / n) << endl;
+cout << endl << "Najmniejszy element tablicy to: " << min << endl;
+cout << "Najwiekszy element tablicy to: " << max << endl;
+cout << endl << "W tablicy jest " << dodatnie << " elementow dodatnich." << endl;
+cout << "W tablicy jest " << ujemne << " elementow ujemnych." << endl;
+cout << "W tablicy jest " << zerowe << " elementow zerowych." << endl;
+
+char znak;
+cin >> znak;
 }
